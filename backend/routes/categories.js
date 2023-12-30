b49a7337548f75fd3a3af946a1fa421258487dc5
Fidan Haliti <fidan91haliti@gmail.com>
2023-12-30 17:40:47 +0100

    read,update,delete,user-create

--- backend/routes/categories.js
+++ backend/routes/categories.js
@@ -2,6 +2,7 @@ const express = require("express");
 const router = express.Router();
 const Category = require("../models/Category.js");
 
+// Get new categories
 router.post("/", async (req, res) => {
   try {
     const { name, img } = req.body;
@@ -16,7 +17,72 @@ router.post("/", async (req, res) => {
 }),
   // Gjitha kategorite (Read All)
   router.get("/", async (req, res) => {
-    res.send("Category Send!");
+    try {
+      const categories = await Category.find();
+
+      res.status(200).json(categories);
+    } catch (error) {
+      console.log(error);
+      res.status(500).json({ error: "Server error." });
+    }
   });
+
+//vetem nje kategori te caktuar (Read-Single)
+router.get("/:categoryId", async (req, res) => {
+  try {
+    const categoryId = req.params.categoryId;
+    try {
+      const category = await Category.findById(categoryId);
+      res.status(200).json(category);
+    } catch (error) {
+      console.log(error);
+      res.status(400).json({ error: "Category not found" });
+    }
+  } catch (err) {
+    console.log(err);
+    res.status(500).json({ error: "Server error Fidan." });
+  }
+});
+
+// Update kategory (Update)
+router.put("/:categoryId", async (req, res) => {
+  try {
+    const categoryId = req.params.categoryId;
+    const updates = req.body;
+
+    const existingCategory = await Category.findById(categoryId);
+    if (!existingCategory) {
+      return res.status(404).json({ error: "Category not found." });
+    }
+
+    const updateCategory = await Category.findByIdAndUpdate(
+      categoryId,
+      updates,
+      {
+        new: true,
+      }
+    );
+    res.status(200).json(updateCategory);
+  } catch (err) {
+    console.log(err);
+    res.status(500).json({ error: "Server error." });
+  }
+});
+
+//Fshij Kategorin (Delete)
+router.delete("/:categoryId", async (req, res) => {
+  try {
+    const categoryId = req.params.categoryId;
+
+    const deleteCategory = await Category.findByIdAndDelete(categoryId);
+    if (!deleteCategory) {
+      return res.status(404).json({ error: "Category not found." });
+    }
+    res.status(200).json(deleteCategory);
+  } catch (error) {
+    console.log(error);
+    res.status(400).json({ error: "Failed to Delete." });
+  }
+});
 
 module.exports = router;
--- backend/routes/index.js
+++ backend/routes/index.js
@@ -3,10 +3,12 @@ const router = express.Router();
 
 // integrimi i dosjave tjera ne rute 
 const productRoute = require("./products.js");
+const authRoute = require("./auth.js");
 const categoryRoute = require("./categories.js");
 
 //gjdo rute te jete ne rrugen e saje 
 router.use("/categories", categoryRoute);
+router.use("/auth", authRoute);
 router.use("/products", productRoute);
 
 module.exports = router;
