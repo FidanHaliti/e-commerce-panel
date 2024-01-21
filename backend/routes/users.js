a89e3d7445a040dafe61885dda36a4a8db5ae1e3
Fidan Haliti <fidan91haliti@gmail.com>
2024-01-21 21:37:18 +0100

    Video 77

--- backend/routes/users.js
+++ backend/routes/users.js
@@ -17,4 +17,20 @@ router.get("/", async (req, res) => {
     }
   });
 
+//Fshije Userin (Delete)
+
+router.delete("/:email", async (req, res) => {
+  try {
+    const email = req.params.email;
+
+    const deletedUser = await User.findOneAndDelete({email});
+    if (!deletedUser) {
+      return res.status(404).json({ error: "email not found." });
+    }
+    res.status(200).json(deletedUser);
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({ error: "email to Delete." });
+  }
+});
   module.exports = router;
--- frontend/src/pages/Admin/AdminUserPage.jsx
+++ frontend/src/pages/Admin/AdminUserPage.jsx
@@ -1,75 +1,102 @@
-import { Space, Table, message } from "antd";
+import { Button, Popconfirm, Space, Table, message } from "antd";
+
 import { useCallback, useEffect, useState } from "react";
 
-
-
 const AdminUserPage = () => {
-    const [dataSource, setDataSource] = useState([])
-    const [loading, setLoading] = useState(false)
-  const apiUrl = import.meta.env.VITE_API_BASE_URL
+  const [dataSource, setDataSource] = useState([]);
+  const [loading, setLoading] = useState(false);
+  const apiUrl = import.meta.env.VITE_API_BASE_URL;
   const handleChange = (pagination, filters, sorter) => {
     console.log("Various parameters", pagination, filters, sorter);
-   
   };
- 
- 
+
   const columns = [
     {
       title: "Avatar",
       dataIndex: "avatar",
       key: "avatar",
       render: (imgSrc) => (
-        <img src={imgSrc}
-        alt="avatar"
-        style={{ width: '50px', height: '50px', borderRadius:"50%", }} />
-        ),
+        <img
+          src={imgSrc}
+          alt="avatar"
+          style={{ width: "50px", height: "50px", borderRadius: "50%" }}
+        />
+      ),
     },
     {
       title: "Username",
       dataIndex: "username",
       key: "username",
-     
     },
-   
+
     {
       title: "Email",
       dataIndex: "email",
       key: "email",
-     
-      
     },
     {
       title: "Role",
       dataIndex: "role",
       key: "role",
     },
+    {
+      title: "Actions",
+      dataIndex: "actions",
+      key: "actions",
+      render: (_, record) => (
+        <Popconfirm
+          title="Delete the user"
+          description="Are you sure to delete this user?"
+          okText="Yes"
+          cancelText="No"
+          onConfirm={() => {
+            deleteUser(record.email);
+          }}
+        >
+          <Button danger>Delete</Button>
+        </Popconfirm>
+      ),
+    },
   ];
 
-  const fetchUsers = useCallback (async () => {
-   setLoading(true);
+  const fetchUsers = useCallback(async () => {
+    setLoading(true);
     try {
-      const response = await fetch(`${apiUrl}/api/users`)
-       
+      const response = await fetch(`${apiUrl}/api/users`);
 
       if (response.ok) {
         const data = await response.json();
-        setDataSource(data)
-    
+        setDataSource(data);
       } else {
         message.error("Login Failed");
       }
     } catch (error) {
       console.log(error);
-    }finally{
-        setLoading(false);
+    } finally {
+      setLoading(false);
     }
   }, [apiUrl]);
 
+  const deleteUser = async (userEmail) => {
+    try {
+      const response = await fetch(`${apiUrl}/api/users/${userEmail}`, {
+        method: "DELETE",
+      });
+
+      if (response.ok) {
+        message.success("Delteted is Succes");
+        fetchUsers();
+      } else {
+        message.error("Delete Error");
+      }
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
   useEffect(() => {
-    fetchUsers()
-  },[fetchUsers])
-
-
+    fetchUsers();
+  }, [fetchUsers]);
 
   return (
     <>
@@ -77,11 +104,14 @@ const AdminUserPage = () => {
         style={{
           marginBottom: 16,
         }}
-      >
-        
+      ></Space>
+      <Table
+        columns={columns}
+        dataSource={dataSource}
       
-      </Space>
-      <Table columns={columns} dataSource={dataSource} onChange={handleChange} rowKey={(record) => record._id} loading={loading} />
+        rowKey={(record) => record._id}
+        loading={loading}
+      />
     </>
   );
 };
